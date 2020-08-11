b693bb2508ee1c2475adb2db1f31a904ada6562d
ryota kobayashi <ryotakobayashi@koba.local>
2020-08-11 13:36:37 +0900

    firebaseconfig setting

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0kVADTmxsJ-dmZhe7iBSbZ4YjMHV1GwQ',
+  authDomain: 'chatbot-34d23.firebaseapp.com',
+  databaseURL: 'https://chatbot-34d23.firebaseio.com',
+  projectId: 'chatbot-34d23',
+  storageBucket: 'chatbot-34d23.appspot.com',
+  messagingSenderId: '653135027105',
+  appId: '1:653135027105:web:b15a7652b9538c3750c9e0',
+  measurementId: 'G-DT25625B8E',
+}
+
+export default firebaseConfig
